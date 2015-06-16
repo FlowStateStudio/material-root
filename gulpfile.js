ccc1534b882fc2f5dcbb409304d2c32222739405
ohall <ohall@grassroots-tech.com>
2015-06-16 11:30:17 -0400

    es6 transpilation and initial build

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+/**
+ * Created by Oakley Hall on 6/16/15.
+ */
+
+var gulp = require('gulp');
+var browserify = require('browserify');
+var babelify = require('babelify');
+var source = require('vinyl-source-stream');
+
+gulp.task('build', function () {
+  browserify({
+    entries: 'src/index.jsx',
+    extensions: ['.jsx'],
+    debug: true
+  })
+    .transform(babelify)
+    .bundle()
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('assets', function() {
+  gulp.src('src/index.html')
+    .pipe(gulp.dest('dist'));
+});
+
+
+gulp.task('default', ['build', 'assets']);
---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+/**
+ * Created by Oakley Hall on 6/16/15.
+ */
+
+import React from 'react';
+
+const thingy = 'THIS ONE!';
+
+React.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('main')
+);
